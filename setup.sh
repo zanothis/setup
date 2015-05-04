e19a93cb3630ccbce16a7a7368173657798e292a
Nathan Benjamin <nb-accounts@thebenjamins.me>
2015-05-04 10:04:54 -0500

    Updated to check for command existence before installation. Changed the way that github repos are pulled down.

--- setup.sh
+++ setup.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-sudo apt-get install -y git-core
-sudo apt-get install -y curl
-
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-
-heroku login
-
 sudo apt-get update
-sudo apt-get install -y python-software-properties python g++ make
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y nodejs
 
-npm install -g jshint
+if [ ! `which git` ]; then
+  sudo apt-get install -y git-core
+fi
 
-git config --global user.name "Nathan Benjamin"
-git config --global user.email "nb-accounts@thebenjamins.me"
+if [ ! `which curl` ]; then
+  sudo apt-get install -y curl
+fi
 
-git clone git@github.com:zanothis/dotfiles.git
+#wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+#heroku login
+#
+#sudo apt-get install -y python-software-properties python g++ make
+#sudo add-apt-repository ppa:chris-lea/node.js
+#sudo apt-get update
+#sudo apt-get install -y nodejs
+#
+#npm install -g jshint
+#
+#git config --global user.name "Nathan Benjamin"
+#git config --global user.email "nb-accounts@thebenjamins.me"
+
+git clone git://github.com/zanothis/dotfiles.git
 cp dotfiles/.screenrc ~
 cp dotfiles/.vimrc ~
 
-git clone git@github.com:zanothis/vim-packages.git
-mkdir .vim
+git clone git://github.com/zanothis/vim-packages.git
+
+mkdir -p ~/.vim
+
 cd vim-packages; cp -rf * ~/.vim
